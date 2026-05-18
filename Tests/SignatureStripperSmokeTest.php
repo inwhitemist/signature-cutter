b92da3eb844c2b9f8d148a943c2e5bde2fadac0e
inwhitemist <52346013+inwhitemist@users.noreply.github.com>
2026-05-18 15:48:41 +0300

    Update

--- Tests/SignatureStripperSmokeTest.php
+++ Tests/SignatureStripperSmokeTest.php
@@ -8,15 +8,15 @@ $stripper = new SignatureStripper(require __DIR__.'/../Config/config.php');
 
 $cases = [
     [
-        "Коллеги, контейнер в статусе Released.\n\nС уважением,\n\nМихаил.\n\nMikhail AUMEYSTERS | Shift Masters | Shift master\n\nAGRANA Fruit Moscow region LLC | Festivalnaya street, 5 | 142214 Serpukhov, Russia\nPhone: +7 (4967) 76-09-70 (Ext. 56824)\nMobile: +7 (985) 111-31-65\nE-Mail: Mikhail.AUMEYSTERS@agrana.com | https://www.agrana.com | Privacy Principles\n\nLogo\n\nDisclaimer: This message contains confidential information and is solely intended for the addressee(s).\n\nAgrana Fruit in Fashion Banner",
+        "Коллеги, контейнер в статусе Released.\n\nС уважением,\n\nМихаил.\n\nM | Shift Masters | Shift master\n\nAGRANA Fruit Moscow region LLC | Festivalnaya street, 5 | 142214 Serpukhov, Russia\nPhone: \nE-Mail:  | https://www.agrana.com | Privacy Principles\n\nLogo\n\nDisclaimer: This message contains confidential information and is solely intended for the addressee(s).\n\nAgrana Fruit in Fashion Banner",
         "Коллеги, контейнер в статусе Released.",
     ],
     [
-        "Ок, принято.\r\n\r\nС Уважением,\r\n\r\nMaksim ELTSOW | Line leader | T: +7 (4967) 76-09-70 (Ext. 56886) | M: +7 (985) 080-50-70",
+        "Ок, принято.\r\n\r\nС Уважением,\r\n\r\n | Line leader |",
         "Ок, принято.",
     ],
     [
-        "Новый комментарий.\n\nFrom: AUMEYSTERS Mikhail <Mikhail.AUMEYSTERS@agrana.com>\nSent: Saturday, May 16, 2026 11:11 PM\nTo: ELTSOW Maksim <Maksim.ELTSOW@agrana.com>\nCc: RUSV_SHIFTMASTER <RUSV_SHIFTMASTER@agrana.com>\nSubject: Статус контейнера\n\nold text",
+        "Новый комментарий.\n\nFrom: \nSent: Saturday, May 16, 2026 11:11 PM\nTo:\nCc: \nSubject: Статус контейнера\n\nold text",
         "Новый комментарий.",
     ],
 ];
